020d318d31d9a61e937c6ce69f9818c5cff4e068
eli-sauvage <elicolh@gmail.com>
2024-10-02 16:35:26 +0200

    forgot prop

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
 <script setup lang="ts">
-import Content from './components/Content.vue';
-import HelloWorld from './components/HelloWorld.vue'
-import SideMenu from './components/SideMenu.vue';
-import languages from './components/languages.vue';
-import WelcomePage from './components/WelcomePage.vue'
-import { Ref, ref, VueElement } from 'vue'
-import { langName } from "./ts/languages"
-import CV from "./components/CV.vue"
+import Content from "./components/Content.vue";
+import HelloWorld from "./components/HelloWorld.vue";
+import SideMenu from "./components/SideMenu.vue";
+import languages from "./components/languages.vue";
+import WelcomePage from "./components/WelcomePage.vue";
+import { Ref, ref, VueElement } from "vue";
+import { langName } from "./ts/languages";
+import CV from "./components/CV.vue";
 
-let lang= ref("fr") as Ref<langName>
+let lang = ref("fr") as Ref<langName>;
 function changeLanguage(language: "fr" | "en") {
-  lang.value = language
+  lang.value = language;
 }
 </script>
 
 <template>
   <languages :lang="lang" @changeLanguage="changeLanguage"></languages>
-  <CV/>
-  <WelcomePage :lang="lang"/>
+  <CV :lang="lang" />
+  <WelcomePage :lang="lang" />
   <div id="global">
     <SideMenu :lang="lang" />
-    <Content :lang="lang"/>
+    <Content :lang="lang" />
   </div>
 </template>
 
@@ -31,6 +31,4 @@ function changeLanguage(language: "fr" | "en") {
 #global
     display: flex
 </style>
-<style>
-
-</style>
+<style></style>
